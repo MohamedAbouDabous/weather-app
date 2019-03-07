8b1abba1235884855e550633fdde50d872b50182
MohamedAbouDabous <mohamed.abou-dabous@chasacademy.se>
2019-03-08 00:15:47 +0100

    connected the geolocation with my api request. Getting my locations weather.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,70 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+
+
 
 class App extends Component {
+
+  state = {
+    long: '',
+    lat: ''
+  }
+  
+  componentDidMount() {
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {axios.get(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/93ce24f5d5831a1b6eb47d04de207dda/${position.coords.latitude},${position.coords.longitude}`)
+      .then(json => console.log(json))
+    }
+      )}
+  }
+
+  // componentDidMount(){
+
+  //   if (navigator.geolocation) {
+  //   axios.get(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/93ce24f5d5831a1b6eb47d04de207dda/${this.position.coords.latitude},${this.position.coords.longitude}`)
+  //   .then(json => console.log(json))
+
+  //   }
+  //     if (navigator.geolocation) {
+  //       navigator.geolocation.getCurrentPosition(this.showPosition);
+  //     }
+    
+  // }
+  
+
+  // showPosition(position) {
+  //   console.log(position.coords.latitude, position.coords.longitude);
+  // }
+
+
+
+//   componentDidMount(){
+//     if (navigator.geolocation) {
+//       navigator.geolocation.getCurrentPosition(position);
+//     axios.get(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/93ce24f5d5831a1b6eb47d04de207dda/${this.state.lat},${this.state.long}`)
+//     .then(json => console.log(json).
+//     this.setState ({
+//       long: position.coords.latitude,
+//       lat: position.coords.latitude
+//     })
+//   }
+// }
+
+
+    
+    
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        
       </div>
     );
   }
+
+
 }
+
 
 export default App;
